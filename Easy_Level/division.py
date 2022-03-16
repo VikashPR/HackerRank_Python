073f966d9d9c39146a458cdafdd4ec36824005a2
VikashPR <vikashrajendran2806@gmail.com>
2022-03-16 08:36:31 +0530

    Feat: division add

---
+++ Easy_Level/division.py
@@ -0,0 +1,11 @@
+from __future__ import division
+
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+# Solution starts
+    one = a // b
+    two = a / b
+    print(one);
+    print(two)
+# Solution Ends
