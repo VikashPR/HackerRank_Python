4e9266fd801c38c128e22e02add9a605ee95a11d
VikashPR <vikashrajendran2806@gmail.com>
2023-08-01 23:35:11 +0530

    refactor: old files remove

--- Easy/listComprehensions.py
+++
@@ -1,8 +0,0 @@
-if __name__ == '__main__':
-    x = int(raw_input())
-    y = int(raw_input())
-    z = int(raw_input())
-    n = int(raw_input())
-
-    print([[a, b, c] for a in range(x+1) for b in range(y+1)
-          for c in range(z+1) if a+b+c != n])
